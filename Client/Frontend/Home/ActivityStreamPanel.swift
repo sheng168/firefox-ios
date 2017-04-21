863bbe85f84896e9b07d926d385e041731999ed8
Stephan Leroux <sleroux@mozilla.com>
2017-04-21 12:00:21 -0400

    Fixup: Put back in isInitialLoad to properly show highlights intro

--- Client/Frontend/Home/ActivityStreamPanel.swift
+++ Client/Frontend/Home/ActivityStreamPanel.swift
@@ -368,6 +368,7 @@ extension ActivityStreamPanel: ActivityStreamDataDelegate {
     // See ActivityStreamDataObserver for invalidation logic.
     func reloadAll() {
         accumulate([self.getHighlights, self.getTopSites]).uponQueue(DispatchQueue.main) { _ in
+            self.isInitialLoad = false
             self.collectionView?.reloadData()
         }
     }
